efe1a49c7fcf1895c40f896eb2eb6b3d52e6d2d9
Chris Harris <charris@ingenuity.com>
2014-12-10 13:06:25 -0500

    changes for multiple files and better help

--- status.py
+++ status.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Simple Script to check status of package", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--server', action="store", dest="server", default="https://api-stable.ingenuity.com/datastream/api/v1/", help="url of upload endpoint")
     parser.add_argument('--status_url', action="store", dest="status_url", help="status_url_of_package")
+    parser.add_argument('--dp_id', action="store", dest="dp_id", help="DP_ID of package")
     parser.add_argument('--client-secret', action="store", default=secret, dest="secret", help="supply client secret on the command line, or set an environment variable named ING_CLIENT_SECRET")
     parser.add_argument('--client-id', action="store", default=client_id, dest="client_id", help="supply client id on the command, or set an environment variable named ING_CLIENT_ID")
     parser.add_argument('--logging-level', action="store", dest="log_level", default="WARNING", help="supplying debug will also start file logging for convenience")
@@ -36,9 +37,12 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         CLIENT_ID=args.client_id
-    if args.status_url==None:
+    if args.status_url==None and args.dp_id==None:
         parser.print_help()
         print >>sys.stderr, "\n\nERROR:Please supply a valid filename, %s does not appear to be a valid file" % args.pkg
         sys.exit(1)
-
-    main(args.server, args.status_url, args.log_level)
+    if args.dp_id:
+        status_url = "https://api-stable.ingenuity.com/v1/datapackages/"  + args.dp_id
+    else:
+        status_url = args.status_url
+    main(args.server, status_url, args.log_level)
