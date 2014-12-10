a1dbf82ddd551db199195d00ee385b71198a28d2
Chris Harris <charris@ingenuity.com>
2014-12-10 16:04:35 -0500

    new setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
-setup(name='ingapi',
-        version='0.1',
+setup(name='dsapi',
+        version='0.4',
         description='Ingenuity Upload and Status Python Helper scripts and DatastreamAPI library',
         url='localhost', #FIXME
         author="Chris Harris",
         author_email="charris@bigelow.org",
         license="GPLv3",
-        packages=['ingapi'],
+        packages=['dsapi'],
         install_requires=['requests',
             'logging',
             ],
 
-        scripts=['bin/upload.py',
-                 'bin/status.py',
+        scripts=['bin/ds_upload.py',
+                 'bin/ds_status.py',
                     ],
         zip_safe=False)
